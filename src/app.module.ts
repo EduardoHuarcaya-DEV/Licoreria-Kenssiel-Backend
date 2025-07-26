eb00d081158f5ff7b1f09a6f3e85cc90b8e6a705
EduardoHuarcaya-DEV <waltereduardohm@gmail.com>
2025-07-26 12:30:57 -0500

    Iniciando proyecto - Instalando dependencias

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    //Configuracion de variables de entorno de
+    //la aplicacion, se carga de forma global
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    //Configuracion de TypeORM para la base de datos
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
