3408b8f155eb0551eaf17b7d08b4e86388ba800a
EduardoHuarcaya-DEV <waltereduardohm@gmail.com>
2025-08-23 18:14:20 -0500

    Añadiendo swagger-ui para documentación automatica de API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,7 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-  })
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -19,6 +20,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  // Configurar Swagger
+  const options = new DocumentBuilder()
+    .setTitle('Kenssiel API') // Título de tu API
+    .setDescription('Backend para la gestión de productos en Kenssiel') // Descripción de tu API
+    .setVersion('1.0') // Versión de tu API
+    .addTag('tags') // Puedes añadir etiquetas para clasificar tus endpoints
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/v1', app, document); // Servir la documentación en /api/v1
 
   await app.listen(process.env.PORT ?? 3000);
 }
