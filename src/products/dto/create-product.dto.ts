786ffb1da73ad253ae74e10c6bbd2acf5fa04c8d
EduardoHuarcaya-DEV <waltereduardohm@gmail.com>
2025-08-23 19:05:08 -0500

    ApiProperty a createDto de producto - Primeras impresiones con swagger

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -6,28 +6,36 @@ import {
   IsNotEmpty,
 } from 'class-validator';
 
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateProductDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsNumber()
   @Min(0)
   @IsNotEmpty()
   price: number;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   description: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
-  imageUrl: string;
+  imageUrl?: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   id_category: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   id_supplier: string;
